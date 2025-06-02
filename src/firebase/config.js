b549cdd0264eeafd0157b2172f83f764df1dff65
Deepak sharma <demondark7678@gmail.com>
2025-06-02 20:23:37 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore"; // ← Add this
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "", // Note: fix typo in original
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyAx0tlSB6L7xzLQHzI-u-FCtl7hUIEE468",
+  authDomain: "assignment-1a1f6.firebaseapp.com",
+  projectId: "assignment-1a1f6",
+  storageBucket: "assignment-1a1f6.appspot.com", // Note: fix typo in original
+  messagingSenderId: "848340848409",
+  appId: "1:848340848409:web:64305eb1ffca8801f43f88",
+  measurementId: "G-D4L12ZDZG0"
 };
 
 const app = initializeApp(firebaseConfig);
